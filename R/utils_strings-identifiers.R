173618280e670f6fbbefb44d6101d6006112794e
Jonathan Trattner <accounts@jdtrat.com>
2021-07-26 16:52:24 -0400

    Add string utils for extracting before, after, and between. Originally in jdtrat/jdtratnr.

--- R/utils_strings-identifiers.R
+++ R/utils_strings-identifiers.R
@@ -24,3 +24,89 @@ time_string <- function(length) {
   }
   substr(time, start = (1 + n_time - length), stop = n_time)
 }
+
+#' Extract a portion of a string between two patterns
+#'
+#' If a string has more than one match, this will only return the first one.
+#'
+#' @param full_string The full character string to extract from.
+#' @param after_string A pattern specifying the string should be extracted after this.
+#' @param before_string A pattern specifying the string should be extracted before this.
+#' @param trim_spaces logical T/F, if there are any spaces within the extracted
+#'   string, they will be removed. Default is TRUE.
+#' @return A string
+#' @export
+#'
+#' @examples
+#'
+#' extract_between("What does the fox say?", after_string = "What does the", before_string = "say")
+#'
+extract_between <- function(full_string, after_string, before_string, trim_spaces = TRUE) {
+
+  reg_pattern <- paste0("(?<=", after_string, ").*(?=", before_string, ")")
+
+  out <- regmatches(full_string, gregexpr(pattern = reg_pattern,
+                                          text = full_string,
+                                          perl = T))[[1]]
+
+  if (trim_spaces) {
+    out <- trimws(out)
+  }
+
+  return(out)
+}
+
+
+#' Extract all characters in a string before a match
+#'
+#' If a string has more than one match, this will only return the first one.
+#'
+#' @inheritParams extract_between
+#' @return A string
+#' @export
+#'
+#' @examples
+#'
+#' extract_before("What does the fox say?", before_string = "say")
+#'
+extract_before <- function(full_string, before_string, trim_spaces = TRUE) {
+
+  reg_pattern <- paste0(".*(?=", before_string, ")")
+
+  out <- regmatches(full_string, gregexpr(pattern = reg_pattern,
+                                          text = full_string,
+                                          perl = T))[[1]]
+
+  if(length(out > 1)) out <- out[1]
+  if (trim_spaces) out <- trimws(out)
+
+  return(out)
+}
+
+
+#' Extract all characters in a string after a match
+#'
+#' If a string has more than one match, this will only return the first one.
+#'
+#' @inheritParams extract_between
+#' @return A string
+#' @export
+#'
+#' @examples
+#'
+#' extract_after("What does the fox say?", after_string = "What does the")
+#'
+extract_after <- function(full_string, after_string, trim_spaces = TRUE) {
+
+  reg_pattern <- paste0("(?<=", after_string, ").*")
+
+  out <- regmatches(full_string, gregexpr(pattern = reg_pattern,
+                                          text = full_string,
+                                          perl = T))[[1]]
+
+  if (trim_spaces) {
+    out <- trimws(out)
+  }
+
+  return(out)
+}
--- tests/testthat/test-utils_strings-identifiers.R
+++ tests/testthat/test-utils_strings-identifiers.R
@@ -15,3 +15,51 @@ test_that("time based id increments appropriately", {
   expect_equal(as.numeric(now), as.numeric(now_again ) - 1)
 
 })
+
+
+main_str <- "Telomeres are the protective sheaths on the ends of chromosomes that are responsible for cellular aging."
+before_answer <- "Telomeres are the protective sheaths on the ends of chromosomes that are responsible for"
+after_answer <- "cellular aging."
+between_answer <- "are the protective sheaths on the ends of chromosomes that are responsible for"
+
+test_that("extract_before works", {
+
+  local_edition(3)
+
+  before <- extract_before(full_string = main_str,
+                           before_string = "cellular aging")
+
+  expect_equal(before, before_answer)
+
+
+  before_2 <- extract_before("What does the fox say?", before_string = "say")
+  expect_equal(before_2, "What does the fox")
+
+})
+
+test_that("extract_after works", {
+
+  after <- extract_after(full_string = main_str,
+                         after_string = "responsible for")
+
+  expect_equal(after, after_answer)
+
+  after_2 <- extract_after("What does the fox say?", after_string = "What does the")
+  expect_equal(after_2, "fox say?")
+
+
+})
+
+test_that("extract_between works", {
+
+  between <- extract_between(full_string = main_str,
+                             after_string = "Telomeres",
+                             before_string = "cellular aging")
+
+  expect_equal(between, between_answer)
+
+  between_2 <- extract_between("What does the fox say?", after_string = "What does the", before_string = "say")
+
+  expect_equal(between_2, "fox")
+
+})
