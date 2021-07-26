fdd29abeeefd057af84c7b07fb4d1dc2a879f9ff
Jonathan Trattner <accounts@jdtrat.com>
2021-07-26 17:31:26 -0400

    Add utility functions for creating and opening new files in RStudio Projects.

---
+++ R/utils_new-file.R
@@ -0,0 +1,176 @@
+
+
+get_file_dir <- function(.path) {
+  extract_between(full_string = .path,
+                  after_string = paste0(here::here(), "/"),
+                  before_string = "\\/")
+}
+
+
+create_file <- function(.path, .open) {
+  if (fs::file_exists(.path)) {
+    yes_no("The file {.file {.path}} already exists. Would you like to overwrite it?",
+           yes_action = {
+             fs::file_create(here::here(.path))
+             if (.open) rstudioapi::navigateToFile(.path)
+           },
+           no_action = return(invisible()),
+           no_message = "Okay, not overwriting {.file {.path}}.")
+  }
+  fs::file_create(.path)
+  cli::cli_alert_success("Created {.file {.path}}. Opening now!")
+  if (.open) rstudioapi::navigateToFile(.path)
+}
+
+#' Create a new file
+#'
+#' @description These functions are inspired by [usethis::use_r()] to create .R files in the
+#' R/ directory for packages. Unlike `use_r()`, these functions allow you to
+#' pass in any path within your RStudio Project. It will then create and
+#' (optionally) open the file within RStudio.
+#'
+#' There is both a generic `file_new()` function, which accepts paths and the
+#' file extension, as well as several utility functions that simply require a
+#' path. See usage or examples for more details.
+#'
+#' @param path A character vector of the file path to be created.
+#' @param ext The file extension, e.g. "R" or "Rmd" for .R and .Rmd files,
+#'   respectively.
+#' @param open Logical: whether to open the file for editing. Default calls
+#'   [base::interactive()].
+#'
+#' @return NA; used to create and (optionally) open a new file of the specified
+#'   type.
+#' @export
+#'
+#' @examples
+#'
+#' if (interactive()) {
+#'   # Create a new R file, specifying the extension
+#'   file_new(path = "test-folder/test-file", ext = "R")
+#'
+#'   # Create a new R file with the shortcut:
+#'   new_r(path = "test-folder/test-file")
+#'
+#'   # Create a new Stan file
+#'   file_new(path = "test-folder/test-file", ext = "stan")
+#'   # Alternatively:
+#'   # new_stan(path = "test-folder/test-file")
+#'
+#'   # Create a new JavaScript file
+#'   file_new(path = "test-folder/test-file", ext = "js")
+#'   # Alternatively:
+#'   # new_js(path = "test-folder/test-file")
+#'
+#'   # Create a new CSS file
+#'   file_new(path = "test-folder/test-file", ext = "css")
+#'   # Alternatively:
+#'   # new_css(path = "test-folder/test-file")
+#'
+#'   # Create a new HTML file
+#'   file_new(path = "test-folder/test-file", ext = "html")
+#'   # Alternatively:
+#'   # new_html(path = "test-folder/test-file")
+#'
+#'   # Create a new Text file
+#'   file_new(path = "test-folder/test-file", ext = "txt")
+#'   # Alternatively:
+#'   # new_txt(path = "test-folder/test-file")
+#'
+#'   # Create a new Markdown file
+#'   file_new(path = "test-folder/test-file", ext = "md")
+#'   # Alternatively:
+#'   # new_md(path = "test-folder/test-file")
+#'
+#'   # Create a new RMarkdown file
+#'   file_new(path = "test-folder/test-file", ext = "Rmd")
+#'   # Alternatively:
+#'   # new_rmd(path = "test-folder/test-file")
+#'
+#' }
+#'
+#'
+file_new <- function(path, ext, open = interactive()) {
+
+  path <- here::here(paste0(path, ".", ext))
+
+  file_dir <- get_file_dir(.path = path)
+
+  if (!fs::dir_exists(file_dir)) {
+    yes_no("It looks like you're trying to create a file in a directory {.file {file_dir}} that doesn't exist yet. Would you like to create it?",
+           yes_action = {
+             fs::dir_create(file_dir)
+             create_file(path, open)
+           },
+           yes_message = "Directory {.file {file_dir}} created.",
+           no_message = "Please try creating a file in an existing directory.")
+
+  } else if (fs::dir_exists(file_dir)) {
+    create_file(path, open)
+  }
+
+}
+
+#' @rdname file_new
+#' @export
+file_new_r <- function(path, open = interactive()) {
+  file_new(path = path,
+           ext = "R",
+           open = open)
+}
+
+#' @rdname file_new
+#' @export
+file_new_stan <- function(path, open = interactive()) {
+  file_new(path = path,
+           ext = "stan",
+           open = open)
+}
+
+#' @rdname file_new
+#' @export
+file_new_js <- function(path, open = interactive()) {
+  file_new(path = path,
+           ext = "js",
+           open = open)
+}
+
+#' @rdname file_new
+#' @export
+file_new_css <- function(path, open = interactive()) {
+  file_new(path = path,
+           ext = "css",
+           open = open)
+}
+
+#' @rdname file_new
+#' @export
+file_new_html <- function(path, open = interactive()) {
+  file_new(path = path,
+           ext = "html",
+           open = open)
+}
+
+#' @rdname file_new
+#' @export
+file_new_text <- function(path, open = interactive()) {
+  file_new(path = path,
+           ext = "txt",
+           open = open)
+}
+
+#' @rdname file_new
+#' @export
+file_new_md <- function(path, open = interactive()) {
+  file_new(path = path,
+           ext = "md",
+           open = open)
+}
+
+#' @rdname file_new
+#' @export
+file_new_rmd <- function(path, open = interactive()) {
+  file_new(path = path,
+           ext = "Rmd",
+           open = open)
+}
