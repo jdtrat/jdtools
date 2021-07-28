fcb25dd96d930a0f089e770bb9ee8486828deac7
Jonathan Trattner <accounts@jdtrat.com>
2021-07-28 11:51:11 -0400

    Reorganized code structure and added helper functions for checking if a directory exists and or whether files should be overwritten.

--- R/utils_files.R
+++ R/utils_files.R
@@ -1,201 +1,121 @@
-
-
-get_file_dir <- function(.path) {
-  extract_between(full_string = .path,
-                  after_string = paste0(here::here(), "/"),
-                  before_string = "\\/")
-}
-
-create_file <- function(.path, .open) {
-  if (fs::file_exists(.path)) {
-    yes_no("The file {.file {.path}} already exists. Would you like to overwrite it?",
-           yes_action = {
-             fs::file_create(here::here(.path))
-             if (.open) rstudioapi::navigateToFile(.path)
-           },
-           no_action = return(invisible()),
-           no_message = "Okay, not overwriting {.file {.path}}.")
-  }
-  fs::file_create(.path)
-  cli::cli_alert_success("Created {.file {.path}}. Opening now!")
-  if (.open) rstudioapi::navigateToFile(.path)
-}
-
-# Function factory version of file_new
-file_new_ff <- function(ext) {
-
-  function(path, open = interactive()) {
-    path <- here::here(paste0(path, ".", ext))
-
-    file_dir <- get_file_dir(.path = path)
-
-    if (!fs::dir_exists(file_dir)) {
-      yes_no("It looks like you're trying to create a file in a directory {.file {file_dir}} that doesn't exist yet. Would you like to create it?",
-             yes_action = {
-               fs::dir_create(file_dir)
-               create_file(path, open)
-             },
-             yes_message = "Directory {.file {file_dir}} created.",
-             no_message = "Please try creating a file in an existing directory.")
-
-    } else if (fs::dir_exists(file_dir)) {
-      create_file(path, open)
-    }
-  }
-
-}
-
-#' Create a new file
+#' Check a directory exists
 #'
-#' @description These functions are inspired by [usethis::use_r()] to create .R files in the
-#' R/ directory for packages. Unlike `use_r()`, these functions allow you to
-#' pass in any path within your RStudio Project. It will then create and
-#' (optionally) open the file within RStudio.
+#' This function is a simple utility for checking whether a directory exists. It
+#' is used internally with the `file_new_*` family of functions.
 #'
-#' There is both a generic `file_new()` function, which accepts paths and the
-#' file extension, as well as several utility functions that simply require a
-#' path. See usage or examples for more details.
+#' @param .path The path where an action is to be executed.
 #'
-#' @param path A character vector of the file path to be created.
-#' @param ext The file extension, e.g. "R" or "Rmd" for .R and .Rmd files,
-#'   respectively.
-#' @param open Logical: whether to open the file for editing. Default calls
-#'   [base::interactive()].
-#'
-#' @return NA; used to create and (optionally) open a new file of the specified
-#'   type.
+#' @return LOGICAL; TRUE if the directory already exists  *or* TRUE if the user
+#'   indicates they want to create the directory (in which case it will be
+#'   created in this function). FALSE if the directory does not exist and the
+#'   user indicates they don't want to create it.
 #' @export
 #'
 #' @examples
 #'
 #' if (interactive()) {
-#'   # Create a new R file, specifying the extension
-#'   file_new(path = "test-folder/test-file", ext = "R")
-#'
-#'   # Create a new R file with the shortcut:
-#'   file_new_r(path = "test-folder/test-file")
-#'
-#'   # Create a new Stan file
-#'   file_new(path = "test-folder/test-file", ext = "stan")
-#'   # Alternatively:
-#'   # file_new_stan(path = "test-folder/test-file")
-#'
-#'   # Create a new JavaScript file
-#'   file_new(path = "test-folder/test-file", ext = "js")
-#'   # Alternatively:
-#'   # file_new_js(path = "test-folder/test-file")
-#'
-#'   # Create a new CSS file
-#'   file_new(path = "test-folder/test-file", ext = "css")
-#'   # Alternatively:
-#'   # file_new_css(path = "test-folder/test-file")
-#'
-#'   # Create a new HTML file
-#'   file_new(path = "test-folder/test-file", ext = "html")
-#'   # Alternatively:
-#'   # file_new_html(path = "test-folder/test-file")
-#'
-#'   # Create a new Text file
-#'   file_new(path = "test-folder/test-file", ext = "txt")
-#'   # Alternatively:
-#'   # file_new_text(path = "test-folder/test-file")
-#'
-#'   # Create a new Markdown file
-#'   file_new(path = "test-folder/test-file", ext = "md")
-#'   # Alternatively:
-#'   # file_new_md(path = "test-folder/test-file")
-#'
-#'   # Create a new RMarkdown file
-#'   file_new(path = "test-folder/test-file", ext = "Rmd")
-#'   # Alternatively:
-#'   # file_new_rmd(path = "test-folder/test-file")
-#'
+#'   fs::dir_create("test-folder")
+#'   if (dir_exists_prompt(here("test-folder/test-file.R"))) {
+#'     fs::file_create("test-folder/test-file.R")
+#'   }
 #' }
 #'
-file_new <- function(path, ext, open = interactive()) {
+dir_exists_prompt <- function(.path, exists_action, exists_message) {
 
-  path <- here::here(paste0(path, ".", ext))
-
-  file_dir <- get_file_dir(.path = path)
+  file_dir <- get_file_dir(.path = .path)
 
   if (!fs::dir_exists(file_dir)) {
-    yes_no("It looks like you're trying to create a file in a directory {.file {file_dir}} that doesn't exist yet. Would you like to create it?",
+    yes_no("It looks like you're attempting something in a directory {.file {file_dir}} that doesn't exist yet. Would you like to create it?",
            yes_action = {
              fs::dir_create(file_dir)
-             create_file(path, open)
+             return(TRUE)
            },
-           yes_message = "Directory {.file {file_dir}} created.",
-           no_message = "Please try creating a file in an existing directory.")
-
+           yes_message = "Created directory {.file {file_dir}}.",
+           no_action = return(FALSE),
+           no_message = "Please retry with an existing directory.")
   } else if (fs::dir_exists(file_dir)) {
-    create_file(path, open)
+    return(TRUE)
+  }
+
+}
+
+
+#' Check a file exists and, if so, prompt to overwrite
+#'
+#' This function is a simple utility for checking whether a file exists and
+#' should be overwritten. It is used internally with the `file_new_*` family of
+#' functions.
+#'
+#' @param .path The path where an action is to be executed.
+#'
+#' @return LOGICAL; TRUE if the file does not exist and should be "overwritten"
+#'   *or* TRUE if the user indicates they want to overwrite the file. FALSE if
+#'   the user indicates they don't want to overwrite the file.
+#'
+#' @export
+#'
+#' @examples
+#'
+#' if (interactive()) {
+#'   fs::dir_create("test-folder")
+#'   fs::file_create("test-folder/test-file.R")
+#'   if (overwrite_file("test-folder/test-file.R")) {
+#'     fs::file_create("test-folder/test-file.R")
+#'   }
+#' }
+#'
+overwrite_file <- function(.path) {
+
+  # If file does exist, ask user if they want to overwrite it.
+  if (fs::file_exists(.path)) {
+    yes_no("The file {.file {.path}} already exists. Would you like to overwrite it?",
+           yes_action = return(TRUE),
+           no_action = return(FALSE),
+           no_message = "Okay, not overwriting {.file {.path}}.")
+  }
+
+  # If file doesn't exist, we can "Overwrite" it
+  if (!fs::file_exists(.path)) {
+    return(TRUE)
+  }
+
+}
+
+get_file_dir <- function(.path) {
+  dir <- extract_between(full_string = .path,
+                         after_string = paste0(here::here(), "/"),
+                         before_string = "\\/")
+
+  if (length(dir) == 0) {
+    cli::cli_abort("Parent directory of {.file .path} not detected.")
+  } else {
+    return(dir)
+  }
+
+}
+
+create_file <- function(.path, .open) {
+  if (overwrite_file(.path = .path)) {
+    fs::file_create(.path)
+    cli::cli_alert_success("Created {.file {.path}}. Opening now!")
+    if (.open) rstudioapi::navigateToFile(.path)
   }
 }
 
-#' @rdname file_new
-#' @export
-file_new_r <- file_new_ff("R")
-#'
-#' @rdname file_new
-#' @export
-file_new_stan <- file_new_ff("stan")
+# Does the path passed in already have the extension?
+check_file_ext <- function(.path, .ext) {
 
-#' @rdname file_new
-#' @export
-file_new_js <- file_new_ff("js")
+  path <- here::here(.path)
 
-#' @rdname file_new
-#' @export
-file_new_css <- file_new_ff("css")
+  current_ext <- fs::path_ext(path = path)
 
-#' @rdname file_new
-#' @export
-file_new_html <- file_new_ff("html")
-
-#' @rdname file_new
-#' @export
-file_new_text <- file_new_ff("txt")
-
-#' @rdname file_new
-#' @export
-file_new_md <- file_new_ff("md")
-
-#' @rdname file_new
-#' @export
-file_new_rmd <- file_new_ff("Rmd")
-
-#' Open a File in RStudio
-#'
-#' This function is simply a wrapper around [rstudioapi::navigateToFile()] and
-#' will open the file specified by the `path` argument in the current RStudio
-#' window.
-#'
-#' @param path A character vector of the file to be opened.
-#' @param ... Additional arguments to be passed into
-#'   [rstudioapi::navigateToFile()].
-#'
-#' @return NA; used for side effects to open a file in RStudio.
-#' @export
-#'
-file_open <- function(path, ...) {
-  if (!rstudioapi::isAvailable()) {
-    cli::cli_abort("Oops. Looks like you're not using RStudio.")
+  if (current_ext == .ext) {
+    path
+  } else if (current_ext == "") {
+    paste0(path, ".", .ext)
+  }
+  else if (current_ext != .ext & current_ext != "") {
+    cli::cli_abort("Oops. You want to create a {.file .{.ext}} file, but have specified a path for a file of type {.file .{current_ext}}.")
   }
 
-  path <- here::here(path)
-
-  if (fs::file_exists(path = path)) {
-    rstudioapi::navigateToFile(file = path, ...)
-  } else if (!fs::file_exists(path = path)) {
-    yes_no(
-      prompt = "It looks like you're trying to open a file {.file {path}} that does not exist. Would you like to create it?",
-      yes_action = {
-        fs::file_create(path = path)
-        cli::cli_alert_success("Created {.file {path}}. Opening now!")
-        rstudioapi::navigateToFile(path, ...)
-      },
-      no_action = return(invisible()),
-      no_message = "Okay, not creating {.file {path}}."
-    )
-  }
 }
