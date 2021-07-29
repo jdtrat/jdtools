76bea5b4fa4a1fc8906b18e819256f4ec6a0e03f
Jonathan Trattner <accounts@jdtrat.com>
2021-07-29 16:01:17 -0400

    Rename sub_between() to vec_between()

--- R/func_vec.R
+++ R/func_vec.R
@@ -1,7 +1,7 @@
 #' Subset a vector for values within a specified range
 #'
 #' This function is a modified version of [dplyr::between()]. Unlike `dplyr`'s
-#' version, `sub_between` returns the subset of the supplied vector within the
+#' version, `vec_between` returns the subset of the supplied vector within the
 #' specified boundaries. To mimic the behavior of [dplyr::between()], you can
 #' set `subset = FALSE`.
 #'
@@ -23,31 +23,31 @@
 #' # Create a vector from 0 to 20
 #' x <- seq(1,20)
 #' # Save x_lower as the lower half of the vector
-#' x_lower <- sub_between(vec = x, left = min(x), right = median(x))
+#' x_lower <- vec_between(vec = x, left = min(x), right = median(x))
 #'
 #' # Save x_upper as the upper half of the vector
-#' x_upper <- sub_between(vec = x, left = median(x), right = max(x))
+#' x_upper <- vec_between(vec = x, left = median(x), right = max(x))
 #'
 #' # Combine the vectors and check its equal to the original
-#' # Using all.equal because `sub_between()` coerces to numeric class
+#' # Using all.equal because `vec_between()` coerces to numeric class
 #' all.equal(c(x_lower, x_upper), x)
 #'
 #' # Get the vector for all values between 5 and 10
-#' x5_10 <- sub_between(vec = x, left = 5, right = 10)
+#' x5_10 <- vec_between(vec = x, left = 5, right = 10)
 #'
 #' # To return logical indices, like `dplyr::between()`,
 #' # you can set `subset = FALSE`:
-#' sub_between(vec = x, left = 5, right = 10, subset = FALSE)
+#' vec_between(vec = x, left = 5, right = 10, subset = FALSE)
 #'
 #' # To return the vector, like with `dplyr::between()`, you would subset
 #' # it as follows:
 #'
-#' x_indexed <- x[sub_between(vec = x, left = 5, right = 10, subset = FALSE)]
+#' x_indexed <- x[vec_between(vec = x, left = 5, right = 10, subset = FALSE)]
 #'
 #' # Check that the values are equal
 #' all.equal(x5_10, x_indexed)
 #'
-sub_between <- function(vec, left, right, subset = TRUE) {
+vec_between <- function(vec, left, right, subset = TRUE) {
 
   if (!is.null(attr(vec, "class")) && !inherits(vec, c("Date", "POSIXct"))) {
     warning("vec_between() called on numeric vector with S3 class")
